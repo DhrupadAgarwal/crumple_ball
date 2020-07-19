a230973ae9c4dbceb0ab5e3450d71c68565e993b
DhrupadAgarwal <64142435+DhrupadAgarwal@users.noreply.github.com>
2020-07-19 13:54:52 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -21,7 +21,7 @@ function setup(){
 function keyPressed (){
     if (keyCode === UP_ARROW){
         
-        Matter.body.applyForce(paper.body,paper.body.position,{x:85,y:-85});
+        Matter.Body.applyForce(paper.body,paper.body.position,{x:85,y:-85});
 
     }
 }
